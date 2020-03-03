9bbf1c59bee9ccd605f5ad9917ae231fb3c8ed79
Tien Huynh <thuynh02@uw.edu>
2020-03-02 18:32:35 -0800

    fix

--- SimulationDriver.py
+++ SimulationDriver.py
@@ -1,22 +1,3 @@
 """
 Class which drives the simulation of the smash tournament
 """
-
-
-class Player:
-    def __init__(self):
-        self.distance = 0
-        self.isOut = False
-        self.isPlaying = False
-        self.isLeft = False
-        self.destination = None
-        pass
-
-    def walk(self):
-        pass
-
-    def wait(self):
-        pass
-
-    def play(self):
-        pass
