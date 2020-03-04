5a59164770ef364d9234a6db4de6f04dfd6cca2a
Tien Huynh <thuynh02@uw.edu>
2020-03-03 18:11:39 -0800

    Update test for player

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,17 @@
 import pytest
+from simulationDriver import *
+
+
 def test_1():
-	assert 1+1==2, "Test Passed"
-	
-test_1()
+    assert 1 + 1 == 2, "Test Passed"
+
+
+def test_2():
+    x = SimulationDriver()
+    assert x.time_stamp == 0, "Time stamp = 0"
+    x.begin()
+    assert x.time_stamp == 5, "Time stamp = 5"
+    assert x.total_initial_players == 30, "Initial Players = 30"
+    assert x.players_list[0].player_id == 0
+    assert x.players_list[15].player_id == 15
+    assert x.players_list[29].player_id == 29
