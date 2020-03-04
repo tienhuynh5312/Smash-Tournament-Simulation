8945f6e1175b107e048ad7957fed86658565f995
Tien Huynh <thuynh02@uw.edu>
2020-03-03 18:12:20 -0800

    Skeleton for player class

--- player.py
+++ player.py
@@ -3,23 +3,43 @@ class Player:
     Class to represent the behavior of competitors at smash tournaments
     """
     total_players = 0
+    total_eliminated_players = 0
 
     def __init__(self, player_id=0):
-        Player.totalPlayers = Player.totalPlayers + 1
+        self.play_around_time = 0
+        Player.total_players = Player.total_players + 1
         self.player_id = player_id
         self.walking_distance = 0
         self.isWaiting = True
         self.isPlaying = False
         self.isRecentlyEliminated = False
+        self.current_location = (0, 0)
+        self.destination_location = (0, 0)
 
-    def play_around(self):
+    def walk(self):
+        # - make the player walk toward the destination location
         pass
+
+    def set_destination(self, location_tuple=(0, 0)):
+        self.destination_location = location_tuple
+
+    def eliminated(self):
+        Player.total_eliminated_players = Player.total_eliminated_players + 1
+        self.isPlaying = False
+        self.isWaiting = False
+        self.isRecentlyEliminated = True
 
     def move_random(self):
         pass
 
-    def isRecentlyEliminated(self):
-        return self.isRecentlyEliminated
+    def play_around(self):
+        import numpy as N
+        if self.isRecentlyEliminated:
+            # -  If recently eliminated, give the player random time to stick around
+            self.play_around_time = N.random.normal(30, 10)
+        else:
+            # - now let the player move around for fun.
+            pass
 
     def isWaiting(self):
         return self.isWaiting
