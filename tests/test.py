9a5a9c9756da11e57afd17d02bee5b4b109bf8b7
Tien Huynh <thuynh02@uw.edu>
2020-03-03 23:42:20 -0800

    add test for environment

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from simulationDriver import *
+from environment import *
 
 
 def test_1():
@@ -11,8 +12,16 @@ def test_2():
     x = SimulationDriver()
     assert x.time_stamp == 0, "Time stamp = 0"
     x.begin()
-    assert x.time_stamp == 5, "Time stamp = 5"
+    assert x.time_stamp == SimulationDriver.TIME_STEP, "Time stamp = 5"
     assert x.total_initial_players == 30, "Initial Players = 30"
     assert x.players_list[0].player_id == 0
     assert x.players_list[15].player_id == 15
     assert x.players_list[29].player_id == 29
+
+
+def test_3():
+    x = Environment(30, 30)
+    assert x.env["occupied"].shape == (30,30)
+    assert x.env["organizers"].shape == (30, 30)
+    assert x.env["consoles"].shape == (30, 30)
+    assert x.env["players"].shape == (30, 30)
