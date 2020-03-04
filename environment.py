322d318ce97c19549ff1b900187adacffff49fa9
Tien Huynh <thuynh02@uw.edu>
2020-03-03 23:42:41 -0800

    create environment

--- environment.py
+++ environment.py
@@ -1,5 +1,41 @@
-"""
-Class to represent the enviornment of smash tournaments
-This includes the console play area and areas where the player is waiting
-between matches.
-"""
+class Environment:
+    """
+    Class to represent the environment of smash tournaments
+    This includes the console play area and areas where the player is waiting
+    between matches.
+    """
+
+    def __init__(self, m, n):
+        import numpy as np
+        self.rows = m
+        self.cols = n
+
+        self.__occupied_array = np.zeros((m, n))
+
+        self.__organizers_array = np.zeros((m, n))
+
+        self.__consoles_array = np.zeros((m, n))
+
+        self.__players_array = np.zeros((m, n))
+
+        self.__generate_wall()
+
+        # insert array into one env hashtable for easy access
+        self.env = {"occupied": self.__occupied_array,
+                    "organizers": self.__organizers_array,
+                    "consoles": self.__consoles_array,
+                    "players": self.__players_array}
+
+    def __generate_wall(self):
+        import numpy as np
+        from simulationDriver import SimulationDriver
+
+        door = np.zeros(SimulationDriver.DOOR_LENGTH)
+
+        wall = np.ones(self.cols)
+
+        # TODO: Improve generating door
+        wall[0:len(door)] = door
+
+        # Mark occupied
+        self.__occupied_array[SimulationDriver.WALL_ROW, :] = wall
