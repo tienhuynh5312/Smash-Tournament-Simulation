3f287cbc1c91124452b3f60775c329cd0dd99247
Tien Huynh <thuynh02@uw.edu>
2020-03-07 00:52:45 -0800

    add caculate distance between two points

--- Utility.py
+++ Utility.py
@@ -1,6 +1,16 @@
 DEBUG_MODE = True
+import numpy as np
 
 
 def print_debug(str_in, end="\n"):
     if DEBUG_MODE:
         print(str_in, end=end)
+
+
+def distance(location_a, location_b):
+    import math
+    return math.sqrt((location_a[0] - location_b[0]) ** 2 + (location_a[1] - location_b[1]) ** 2)
+
+
+def random():
+    return np.random.random()
