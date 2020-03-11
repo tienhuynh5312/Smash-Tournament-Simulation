1fe79e19b2b8c8dc90736407a987b637e0f9e996
Tien Huynh <tienhuynh5312@gmail.com>
2020-03-11 16:19:28 -0700

    Update Utility.py

--- Utility.py
+++ Utility.py
@@ -14,3 +14,24 @@ def distance(location_a, location_b):
 
 def random():
     return np.random.random()
+
+
+def get_random_location_waiting_area(env):
+    from simulationDriver import SimulationDriver
+
+    row_min = SimulationDriver.WALL_ROW + 1
+    row_max = SimulationDriver.ALL_AREA_ROWS
+    col_min = 0
+    col_max = SimulationDriver.ALL_AREA_COLS
+
+    random_location_row = np.random.randint(row_min, row_max)
+    random_location_col = np.random.randint(col_min, col_max)
+    random_location = (random_location_row, random_location_col)
+
+    # if there is something occupied at random_location, generate another one.
+    while env.env["occupied"][random_location] == 1:
+        random_location_row = np.random.randint(row_min, row_max)
+        random_location_col = np.random.randint(col_min, col_max)
+        random_location = (random_location_row, random_location_col)
+
+    return random_location
